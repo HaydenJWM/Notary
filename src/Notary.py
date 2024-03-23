8092de6ceed544a280fc4f41fc2903823e846cdc
Hayden Mirza <haydenjwmirza@gmail.com>
2024-03-23 13:23:07 -0400

    Imported the correct widget

--- src/Notary.py
+++ src/Notary.py
@@ -1,16 +1,16 @@
 from textual import events
 from textual.app import App, ComposeResult
-from textual.widgets import Header, RichLog
+from textual.widgets import Header, TextArea, Footer 
 
 class main(App[str]):
     TITLE = "Notary"
     
     def compose(self) -> ComposeResult:
         yield Header()
-        yield RichLog()
+        yield TextArea()
+        yield Footer()
 
-    def on_key(self, event: events.Key) -> None:
-        self.query_one(RichLog).write(event)
+    
 
 
 if __name__ == "__main__":
