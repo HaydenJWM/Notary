5ec5d0980128e2e617a5888a2577308fae622889
Hayden Mirza <haydenjwmirza@gmail.com>
2024-03-24 17:15:42 -0400

    simple command line args

--- src/Notary.py
+++ src/Notary.py
@@ -4,38 +4,39 @@ from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.widgets import Header, TextArea, Footer 
 
-class main(App[str]):
+class Notary(App[str]):
     BINDINGS = [
         Binding("ctrl+q", "quit", "Quit Notary"),
         Binding("ctrl+s", "save()", "Save Note"),     
         Binding("ctrl+o", "open()", "Open Note")
     ]
-
     TITLE = "Notary"
-   
     noteArea = None
+    filename = None
+
+    def initFilename(self):
+        #If filename was given in the args use it
+        if(len(sys.argv) >= 2):
+            self.filename = sys.argv[1]
+
 
     def compose(self) -> ComposeResult:
         self.noteArea = TextArea()
+        self.initFilename()
+
         yield Header()
         yield self.noteArea
         yield Footer()
-    
+
     def action_open(self) -> None:
         print("placeholder")
     
     def action_save(self) -> None:
-        f = open("HelloWorld.txt", "x")
+        f = open(self.filename, "x")
         f.write(self.noteArea.text)
         f.close()
 
-if __name__ == "__main__":
-    filename = ""
-
-    #If a filename was given in the command line then open it
-    if(len(sys.argv) >= 2):
-        filename = sys.argv[1]
-        
-    app = main()
+if __name__ == "__main__": 
+    app = Notary()
     app.run()
     
